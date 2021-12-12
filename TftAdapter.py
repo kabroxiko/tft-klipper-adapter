84c12a73b0f2607263816bb42305fc298de0c873
vojjta <ota.sverma@gmail.com>
2021-12-12 18:57:31 +0100

    added M220 and M221 support

--- TftAdapter.py
+++ TftAdapter.py
@@ -3,20 +3,25 @@ import atexit
 import requests  # instalation python -m pip install requests
 import threading
 
-TEMP_URL = "http://192.168.100.151/printer/objects/query?extruder=target,temperature&heater_bed=target,temperature"
+ADDRESS = "http://192.168.100.151/"
 
-POSITION_URL = "http://192.168.100.151/printer/objects/query?gcode_move=gcode_position"
+TEMP_URL = ADDRESS+"printer/objects/query?extruder=target,temperature&heater_bed=target,temperature"
+POSITION_URL = ADDRESS+"printer/objects/query?gcode_move=gcode_position"
+SPEED_FACTOR_URL = ADDRESS+"printer/objects/query?gcode_move=speed_factor"
+EXTRUDE_FACTOR_URL = ADDRESS+"printer/objects/query?gcode_move=extrude_factor"
 
-gcode_url_template = "http://192.168.100.151/printer/gcode/script?script={g:s}"
 
+gcode_url_template = ADDRESS+"printer/gcode/script?script={g:s}"
+temp_template = "ok T:{ETemp:.4f} /{ETarget:.4f} B:{BTemp:.4f} /{BTarget:.4f} P:0 /0.0000 @:0 B@:0\n"
+position_template = "X:{x:.2f} Y:{y:.2f} Z:{z:.2f} E:{e:.2f} \nok\n"
+feed_rate_template = "FR:{fr:}%\nok\n"
+flow_rate_template = "E0 Flow: {er:}%\nok\n"
 
 
 ser = serial.Serial('/dev/ttyAMA0', 57600)  # open serial port
 
 print(ser.name)
 
-temp_template = "ok T:{ETemp:.4f} /{ETarget:.4f} B:{BTemp:.4f} /{BTarget:.4f} P:0 /0.0000 @:0 B@:0\n"
-position_template = "X:{x:.2f} Y:{y:.2f} Z:{z:.2f} E:{e:.2f} \nok\n"
 
 lock = threading.Lock()
 
@@ -27,7 +32,6 @@ def auto_satus_repost():
   threading.Timer(5.0, auto_satus_repost).start()
   write_to_serial(get_status())
   
-
 def write_to_serial(data):
     if ser.is_open:
         lock.acquire()
@@ -54,6 +58,17 @@ def get_current_position():
     print(position)
     return position_template.format(x=position[0],y=position[1],z=position[2],e=position[3])
 
+def get_speed_factor():
+    r = requests.get(SPEED_FACTOR_URL)
+    print(r.json())
+    speed_factor = r.json().get("result").get("status").get("gcode_move").get("speed_factor")
+    return feed_rate_template.format(fr=speed_factor*100)
+
+def get_extrude_factor():
+    r = requests.get(EXTRUDE_FACTOR_URL)
+    print(r.json())
+    extrude_factor = r.json().get("result").get("status").get("gcode_move").get("extrude_factor")
+    return flow_rate_template.format(er=extrude_factor*100)    
 
 #auto_satus_repost()
 def send_gcode_to_api(gcode):
@@ -90,8 +105,20 @@ while True:
         send_gcode_to_api("G90")
         write_to_serial("ok\n")
         write_to_serial(get_current_position())
-        
-        
+    elif "M220" in gcode.capitalize():    
+        if "M220 S" in gcode.upper():
+            send_gcode_to_api(gcode)
+            write_to_serial("ok\n")
+        else:
+            write_to_serial(get_speed_factor())
+    elif "M221" in gcode.capitalize():    
+        if "M221 S" in gcode.upper():
+            send_gcode_to_api(gcode)
+            write_to_serial("ok\n")
+        else:
+            write_to_serial(get_extrude_factor())
+
+
     else:
         print("default response to serial")
         write_to_serial(get_status())
