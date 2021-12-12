a257831a69cdd55da8029e9795d1b36340ba4961
vojjta <ota.sverma@gmail.com>
2021-12-12 11:52:56 +0100

    added connection to API

--- TftAdapter.py
+++ TftAdapter.py
@@ -1,29 +1,33 @@
+from os import stat
 import serial
 import time
 import atexit
+import requests # instalation python -m pip install requests
 
+TEMP_URL = "http://192.168.100.151/printer/objects/query?extruder=target,temperature&heater_bed=target,temperature"
 
 ser = serial.Serial('/dev/ttyAMA0', 57600)  # open serial port
 
 print(ser.name)
 
+temp_template = "ok T:{ETemp:.4f} /{ETarget:.4f} B:{BTemp:.4f} /{BTarget:.4f} P:0 /0.0000 @:0 B@:0\n"
 
-txt = "ok T:{TCur:.4f} /{TDes:.4f} B:{BCur:.4f} /{TDes:.4f} P:0 /0.0000 @:0 B@:0\n"
+while True:
 
-# print(txt.format(TCur = 20, TDes=190, BCur=20, BDes = 60))
-
-i = 0
-while i < 10:
+    gcode = ser.readline()
+    print("data from serial:\n")
+    print(gcode)
     
-    print(ser.readline())
+    if "M105" in gcode.capitalize():
+        r = requests.get(TEMP_URL)
+        print(r.status_code)
+        print(r.json())
+        status = r.json().get("result").get("status")
+        statusExtruder = status.get("extruder")
+        statusBed = status.get("heater_bed")
+        
+        ser.write(bytes(temp_template.format(ETemp = statusExtruder.get("temperature"), ETarget= statusExtruder.get("target"), BTemp=statusBed.get("temperature"), BTarget = statusBed.get("target"))))
     
-    ser.write(bytes(txt.format(txt.format(TCur = 20, TDes=190, BCur=20, BDes = 60))))
-    
-    i += 1
-    time.sleep(1)
-
-
-
 
 def exit_handler():
     if ser.is_open:
