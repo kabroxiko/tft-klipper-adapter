5248299084a2aa6edc0075cae4411f9a8830b3c9
vojjta <ota.sverma@gmail.com>
2021-12-12 10:58:14 +0100

    small fix

--- TftAdapter.py
+++ TftAdapter.py
@@ -14,19 +14,20 @@ txt = "ok T:{TCur:.4f} /{TDes:.4f} B:{BCur:.4f} /{TDes:.4f} P:0 /0.0000 @:0 B@:0
 
 i = 0
 while i < 10:
-
+    
     print(ser.readline())
-
-    ser.write(bytes((txt.format(TCur=20, TDes=190, BCur=20, BDes=60))))
-
+    
+    ser.write(bytes(txt.format(txt.format(TCur = 20, TDes=190, BCur=20, BDes = 60))))
+    
     i += 1
     time.sleep(1)
+
+
 
 
 def exit_handler():
     if ser.is_open:
         ser.close()
     print("Serial closed")
-
 
 atexit.register(exit_handler)
