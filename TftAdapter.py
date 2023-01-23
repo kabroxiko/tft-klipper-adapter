9c6990761f088041d7053430405bfb3fe1b2579b
vojjta <ota.sverma@gmail.com>
2023-01-23 10:39:41 +0100

    src splitted into files

--- TftAdapter.py
+++ TftAdapter.py
@@ -1,77 +1,57 @@
-import serial
 import atexit
-import requests  # instalation python -m pip install requests
 import threading
+import logging
+from MoonrakerApiClient import MoonrakerApiClient
+from TFTSerial import TFTSerial
 
+logging.basicConfig(level=logging.INFO)
+
+# This should be moved to config
 ADDRESS = "http://192.168.100.240/"
+PORT = '/dev/ttyUSB0'
 
-TEMP_URL = ADDRESS+"printer/objects/query?extruder=target,temperature&heater_bed=target,temperature"
-POSITION_URL = ADDRESS+"printer/objects/query?gcode_move=gcode_position"
-SPEED_FACTOR_URL = ADDRESS+"printer/objects/query?gcode_move=speed_factor"
-EXTRUDE_FACTOR_URL = ADDRESS+"printer/objects/query?gcode_move=extrude_factor"
+client = MoonrakerApiClient(ADDRESS)
+tft_serial = TFTSerial(PORT)
+tft_serial.open()
 
-gcode_url_template = ADDRESS+"printer/gcode/script?script={g:s}"
+
 temp_template = "ok T:{ETemp:.4f} /{ETarget:.4f} B:{BTemp:.4f} /{BTarget:.4f} P:0 /0.0000 @:0 B@:0\n"
 position_template = "X:{x:.2f} Y:{y:.2f} Z:{z:.2f} E:{e:.2f} \nok\n"
 feed_rate_template = "FR:{fr:}%\nok\n"
 flow_rate_template = "E0 Flow: {er:}%\nok\n"
 
-ser = serial.Serial('/dev/ttyUSB0', 57600)  # open serial port
-
-print(ser.name)
-
-lock = threading.Lock()
-
 acceptable_gcode = ["M104", "M140", "M106", "M84"]
 
 # display is asking by M105 for reporting temps
 def auto_satus_repost():
-  threading.Timer(5.0, auto_satus_repost).start()
-  write_to_serial(get_status())
-  
-def write_to_serial(data):
-    if ser.is_open:
-        lock.acquire()
-        try:
-            ser.write(bytes(data, encoding='utf-8'))
-        finally:
-            lock.release()
-    else:
-        print("serial port is not open")
+    threading.Timer(5.0, auto_satus_repost).start()
+    tft_serial.write(get_status())
 
 def get_status():
-    r = requests.get(TEMP_URL)
-    print(r.status_code)
-    print(r.json())
-    status = r.json().get("result").get("status")
+    r = client.get_status()
+    status = r.get("result").get("status")
     statusExtruder = status.get("extruder")
     statusBed = status.get("heater_bed")
-    return temp_template.format(ETemp = statusExtruder.get("temperature"), ETarget= statusExtruder.get("target"), BTemp=statusBed.get("temperature"), BTarget = statusBed.get("target"))
+    return temp_template.format(ETemp=statusExtruder.get("temperature"), ETarget=statusExtruder.get("target"), BTemp=statusBed.get("temperature"), BTarget=statusBed.get("target"))
+
 
 def get_current_position():
-    r = requests.get(POSITION_URL)
-    print(r.json())
-    position = r.json().get("result").get("status").get("gcode_move").get("gcode_position")
-    print(position)
-    return position_template.format(x=position[0],y=position[1],z=position[2],e=position[3])
+    r = client.get_current_position()
+    position = r.get("result").get("status").get("gcode_move").get("gcode_position")
+    logging.info("Position:{}".format(position))
+    return position_template.format(x=position[0], y=position[1], z=position[2], e=position[3])
+
 
 def get_speed_factor():
-    r = requests.get(SPEED_FACTOR_URL)
-    print(r.json())
-    speed_factor = r.json().get("result").get("status").get("gcode_move").get("speed_factor")
+    r = client.get_speed_factor()
+    speed_factor = r.get("result").get("status").get("gcode_move").get("speed_factor")
     return feed_rate_template.format(fr=speed_factor*100)
 
-def get_extrude_factor():
-    r = requests.get(EXTRUDE_FACTOR_URL)
-    print(r.json())
-    extrude_factor = r.json().get("result").get("status").get("gcode_move").get("extrude_factor")
-    return flow_rate_template.format(er=extrude_factor*100)    
 
-#auto_satus_repost()
-def send_gcode_to_api(gcode):
-    r = requests.post(gcode_url_template.format(g=gcode))
-    print(r)
-    return r.json().get("result")
+def get_extrude_factor():
+    r = client.get_extrude_factor()
+    extrude_factor = r.get("result").get("status").get("gcode_move").get("extrude_factor")
+    return flow_rate_template.format(er=extrude_factor*100)
 
 def check_is_basic_gcode(gcode):
     for g in acceptable_gcode:
@@ -79,49 +59,45 @@ def check_is_basic_gcode(gcode):
             return True
     return False
 
-while True:
-
-    gcode = ser.readline().decode("utf-8")
-    print("data from serial:\n")
-    print(gcode)
-    
-    if check_is_basic_gcode(gcode):
-        send_gcode_to_api(gcode)
-        write_to_serial("ok\n")
-    elif "M105" in gcode.capitalize():
-        write_to_serial(get_status())
-    elif "M114" in gcode.capitalize():
-        write_to_serial(get_current_position())
-    elif "G28" in gcode.capitalize():
-        send_gcode_to_api(gcode)
-        write_to_serial("ok\n")
-        write_to_serial(get_current_position())
-    elif "G1" in gcode.capitalize():
-        send_gcode_to_api("G91")
-        send_gcode_to_api(gcode)
-        send_gcode_to_api("G90")
-        write_to_serial("ok\n")
-        # write_to_serial(get_current_position())
-    elif "M220" in gcode.capitalize():    
-        if "M220 S" in gcode.upper():
-            send_gcode_to_api(gcode)
-            write_to_serial("ok\n")
-        else:
-            write_to_serial(get_speed_factor())
-    elif "M221" in gcode.capitalize():    
-        if "M221 S" in gcode.upper():
-            send_gcode_to_api(gcode)
-            write_to_serial("ok\n")
-        else:
-            write_to_serial(get_extrude_factor())
-    else:
-        print("default response to serial")
-        write_to_serial(get_status())
-        
-
 def exit_handler():
-    if ser.is_open:
-        ser.close()
-    print("Serial closed")
+    tft_serial.close()
+    logging.info("Serial closed")
 
 atexit.register(exit_handler)
+
+while True:
+
+    gcode = tft_serial.read()
+
+    if check_is_basic_gcode(gcode):
+        client.send_gcode_to_api(gcode)
+        tft_serial.send_ok()
+    elif "M105" in gcode.capitalize():
+        tft_serial.write(get_status())
+    elif "M114" in gcode.capitalize():
+        tft_serial.write(get_current_position())
+    elif "G28" in gcode.capitalize():
+        client.send_gcode_to_api(gcode)
+        tft_serial.send_ok()
+        tft_serial.write(get_current_position())
+    elif "G1" in gcode.capitalize():
+        client.send_gcode_to_api("G91")
+        client.send_gcode_to_api(gcode)
+        client.send_gcode_to_api("G90")
+        tft_serial.send_ok()
+        # write_to_serial(get_current_position())
+    elif "M220" in gcode.capitalize():
+        if "M220 S" in gcode.upper():
+            client.send_gcode_to_api(gcode)
+            tft_serial.send_ok()
+        else:
+            tft_serial.write(get_speed_factor())
+    elif "M221" in gcode.capitalize():
+        if "M221 S" in gcode.upper():
+            client.send_gcode_to_api(gcode)
+            tft_serial.send_ok()
+        else:
+            tft_serial.write(get_extrude_factor())
+    else:
+        logging.info("Default response to serial for gcode {}".format(gcode))
+        tft_serial.write(get_status())
