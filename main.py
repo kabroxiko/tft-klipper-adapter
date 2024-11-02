2c4b3db918bd89a1dd28e87d3cd021785891c03e
Hugo Araya Nash <kabroxiko@gmail.com>
2024-11-02 00:14:54 +0000

    Se agregan parametros

--- main.py
+++ main.py
@@ -2,19 +2,27 @@
 import tftadapter
 import logging
 
-logging.basicConfig(format='%(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(message)s',level=logging.INFO)
 
+class Printer:
+    def __init__(self):
+        self.reactor = ''
+    def get_reactor(self):
+        return self.reactor
+    def lookup_object(self, name):
+        default = ''
+        return default
 
 class PrinterConfig:
     def __init__(self):
-        self.printer = 'printer'
+        self.printer = Printer()
     def get_printer(self):
         return self.printer
     def get(self, key):
         if key == 'tft_device':
             return '/dev/ttyS2'
         elif key == 'moonraker_url':
-            return 'http://127.0.0.1/'
+            return 'http://127.0.0.1:7125'
         else:
             exit(1)
     def getint(self, key):
--- tftadapter.py
+++ tftadapter.py
@@ -13,29 +13,19 @@ class TFTAdapter:
         tft_baud = config.getint('tft_baud')
         moonraker_url = config.get('moonraker_url')
 
-        query = "query?extruder=target,temperature"
-
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        }
-        query_url = "%sprinter/objects/query" % (moonraker_url)
+        query_url = "%s/printer/objects/query" % (moonraker_url)
         self.temp_url = "%s?extruder=target,temperature&heater_bed=target,temperature" % (query_url)
-        self.POSITION_URL = moonraker_url+"printer/objects/query?gcode_move=gcode_position"
-        self.SPEED_FACTOR_URL = moonraker_url+"printer/objects/query?gcode_move=speed_factor"
-        self.EXTRUDE_FACTOR_URL = moonraker_url+"printer/objects/query?gcode_move=extrude_factor"
-        self.TEMP_URL = moonraker_url+"printer/objects/query"
-        self.gcode_url_template = moonraker_url+"printer/gcode/script?script={g:s}"
-        # self.temp_url = "%s/printer/objects/%s" % (moonraker_url, temp_query)
+        self.position_url = "%s?gcode_move=gcode_position" % (query_url)
+        self.speed_factor_url = "%s?gcode_move=speed_factor" % (query_url)
+        self.extrude_factor_url = "%s?gcode_move=extrude_factor" % (query_url)
+        self.gcode_url_template = "%s/printer/gcode/script?script={g:s}" % (moonraker_url)
 
-        self.temp_template = "ok T:{ETemp:.4f} /{ETarget:.4f} B:{BTemp:.4f} /{BTarget:.4f} P:0 /0.0000 @:0 B@:0\n"
+        self.temp_template = "ok T:{ETemp:.2f} /{ETarget:.2f} B:{BTemp:.2f} /{BTarget:.2f} @:0 B@:0\n"
         self.position_template = "X:{x:.2f} Y:{y:.2f} Z:{z:.2f} E:{e:.2f} \nok\n"
         self.feed_rate_template = "FR:{fr:}%\nok\n"
         self.flow_rate_template = "E0 Flow: {er:}%\nok\n"
 
-        self.tftSerial = serial.Serial(tft_device, tft_baud)  # open serial port
-        # self.ws = create_connection("wss://ws.dogechain.info/inv")
-
-        logging.info(self.tftSerial.name)
+        self.tftSerial = serial.Serial(tft_device, tft_baud)
 
         self.lock = threading.Lock()
 
@@ -55,50 +45,51 @@ class TFTAdapter:
             self.lock.acquire()
             try:
                 self.tftSerial.write(bytes(data, encoding='utf-8'))
+                logging.info("response: %s" % data)
             finally:
                 self.lock.release()
         else:
             logging.info("serial port is not open")
 
     def get_status(self):
-        logging.info(self.temp_url)
+        logging.debug(self.temp_url)
         r = requests.get(self.temp_url)
-        logging.info(r)
-        logging.info(r.status_code)
-        logging.info(r.json())
+        logging.debug(r)
+        logging.debug(r.status_code)
+        logging.debug(r.json())
         results = r.json().get("result")
-        logging.info(results)
+        logging.debug(results)
         status = results.get("status")
-        logging.info(status)
+        logging.debug(status)
         statusExtruder = status.get("extruder")
-        logging.info(statusExtruder)
+        logging.debug(statusExtruder)
         statusBed = status.get("heater_bed")
-        logging.info(statusBed)
+        logging.debug(statusBed)
         return self.temp_template.format(ETemp = statusExtruder.get("temperature"), ETarget= statusExtruder.get("target"), BTemp=statusBed.get("temperature"), BTarget = statusBed.get("target"))
 
     def get_current_position(self):
-        r = requests.get(self.POSITION_URL)
-        logging.info(r.json())
+        r = requests.get(self.position_url)
+        logging.debug(r.json())
         position = r.json().get("result").get("status").get("gcode_move").get("gcode_position")
-        logging.info(position)
+        logging.debug(position)
         return self.position_template.format(x=position[0],y=position[1],z=position[2],e=position[3])
 
     def get_speed_factor(self):
-        r = requests.get(self.SPEED_FACTOR_URL)
-        logging.info(r.json())
+        r = requests.get(self.speed_factor_url)
+        logging.debug(r.json())
         speed_factor = r.json().get("result").get("status").get("gcode_move").get("speed_factor")
         return self.feed_rate_template.format(fr=speed_factor*100)
 
     def get_extrude_factor(self):
-        r = requests.get(self.EXTRUDE_FACTOR_URL)
-        logging.info(r.json())
+        r = requests.get(self.extrude_factor_url)
+        logging.debug(r.json())
         extrude_factor = r.json().get("result").get("status").get("gcode_move").get("extrude_factor")
         return self.flow_rate_template.format(er=extrude_factor*100)
 
     #self.auto_satus_repost()
     def send_gcode_to_api(self, gcode):
         r = requests.post(self.gcode_url_template.format(g=gcode))
-        logging.info(r)
+        logging.debug(r)
         return r.json().get("result")
 
     def check_is_basic_gcode(self, gcode):
@@ -111,14 +102,14 @@ class TFTAdapter:
     def exit_handler(self):
         if self.tftSerial.is_open:
             self.tftSerial.close()
-        logging.info("Serial closed")
+        logging.debug("Serial closed")
 
     def start(self):
-        a = 0
-        while a <= 10:
-            gcode = self.tftSerial.readline().decode("utf-8")
-            logging.info("data from serial:\n")
-            logging.info(gcode)
+        while True:
+            line = self.tftSerial.readline()
+            logging.info("x: '%s'" % line.rstrip())
+            gcode = line.rstrip().decode("utf-8")
+            logging.info("data from serial: %s" % (gcode))
 
             if self.check_is_basic_gcode(gcode):
                 self.send_gcode_to_api(gcode)
@@ -150,9 +141,8 @@ class TFTAdapter:
                 else:
                     self.write_to_serial(self.get_extrude_factor())
             else:
-                logging.info("default response to serial")
+                logging.debug("default response to serial")
                 self.write_to_serial(self.get_status())
-            a += 1
 
 #
 #config loading function of add-on
